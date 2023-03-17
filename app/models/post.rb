7fc91ec5c764ea0b94b4030942feec05f2b8aaf6
ismahaneyy <liciousmayrah@gmail.com>
2023-03-17 06:36:48 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :must_not_have_true_facts
+
+    def must_not_have_true_facts
+        if self.title == "True Facts"
+            errors.add(:title, "Can't contain True Facts")
+        end
+    end
+
 end
